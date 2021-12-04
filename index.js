6113c08ed7614ee28abcc49025920c9b341d0570
Siamak Alipour <siamakalipour37@gmail.com>
2021-12-04 19:45:03 +0330

    new update discord

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 // Require the necessary discord.js classes
-const { Client, Intents, Guild } = require('discord.js');
-const { token } = require('./config.json');
-const { TextChannel } = require('discord.js');
-let cron = require('cron');
+const { Client, Intents } = require('discord.js');
+const { token, botChannelId, adminId } = require('./config.json');
 
 const time = new Date();
 let hour = time.getHours();
@@ -20,82 +18,171 @@ const client = new Client({
 		Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
 	],
 });
-
+// const removeRole = (message, time) => {
+// 	return message.guild.members.cache.map((e) => {
+// 		let hasTime = false;
+// 		e.roles.remove(
+// 			message.guild.roles.cache.find((role) => {
+// 				role.name == time;
+// 				hasTime = true;
+// 			})
+// 		);
+// 		// if (hasTime) {
+// 		// 	e.roles.remove(
+// 		// 		message.guild.roles.cache.find((role) => role.name == `5-Runs`)
+// 		// 	);
+// 		// 	e.roles.remove(
+// 		// 		message.guild.roles.cache.find((role) => role.name == `10-Runs`)
+// 		// 	);
+// 		// 	e.roles.remove(
+// 		// 		message.guild.roles.cache.find((role) => role.name == `15-Runs`)
+// 		// 	);
+// 		// 	e.roles.remove(
+// 		// 		message.guild.roles.cache.find((role) => role.name == `20-Runs`)
+// 		// 	);
+// 		// 	e.roles.remove(
+// 		// 		message.guild.roles.cache.find(
+// 		// 			(role) => role.name == `variable`
+// 		// 		)
+// 		// 	);
+// 		// }
+// 	});
+// };
+// const getMembers = (message, roleName) => {
+// 	return message.guild.members.cache
+// 		.filter((member) => member.roles.cache.find((role) => role == roleName))
+// 		.map((member) => member.user.tag);
+// };
 client.on('message', async (message) => {
-	if (hour >= 15 && hour < 18) {
-		message.guild.members.cache.map((e) => {
-			e.roles.cache.find((role) =>
-				role.name == '15-18' ? e.send('test alert') : null
-			);
-		});
-		message.guild.members.cache.map((e) => {
-			e.roles.remove(
-				message.guild.roles.cache.find((role) => role.name == `12-15`)
-			);
-		});
-		// message.guild.members.cache.find((member) =>
-		// 	member.roles.cache.get('916658100948328500')
-		// );
+	if (
+		message.member.roles.cache.has(adminId) &&
+		message.content.startsWith('check')
+	) {
+		if (hour >= 9 && hour < 12) {
+			message.guild.members.cache.map((e) => {
+				e.roles.cache.find((role) =>
+					role.name == '9-12' ? e.send('test alert') : null
+				);
+			});
+			message.guild.members.cache.map((e) => {
+				e.roles.remove(
+					message.guild.roles.cache.find((role) => {
+						role.name == `0-3`;
+					})
+				);
+			});
+		} else if (hour >= 12 && hour < 15) {
+			message.guild.members.cache.map((e) => {
+				e.roles.cache.find((role) =>
+					role.name == '12-15' ? e.send('test alert') : null
+				);
+			});
+			message.guild.members.cache.map((e) => {
+				e.roles.remove(
+					message.guild.roles.cache.find(
+						(role) => role.name == `9-12`
+					)
+				);
+			});
+		} else if (hour >= 15 && hour < 18) {
+			message.guild.members.cache.map((e) => {
+				e.roles.cache.find((role) =>
+					role.name == '15-18' ? e.send('test alert') : null
+				);
+			});
+			message.guild.members.cache.map((e) => {
+				e.roles.remove(
+					message.guild.roles.cache.find(
+						(role) => role.name == `12-15`
+					)
+				);
+			});
+		} else if (hour >= 18 && hour < 21) {
+			message.guild.members.cache.map((e) => {
+				e.roles.cache.find((role) =>
+					role.name == '18-21' ? e.send('test alert') : null
+				);
+			});
+			message.guild.members.cache.map((e) => {
+				const membersOfTest = e.roles.remove(
+					message.guild.roles.cache.find(
+						(role) => role.name == `15-18`
+					)
+				);
+			});
+		} else if (hour >= 21 && hour < 0) {
+			message.guild.members.cache.map((e) => {
+				e.roles.cache.find((role) =>
+					role.name == '21-0' ? e.send('test alert') : null
+				);
+			});
+			message.guild.members.cache.map((e) => {
+				e.roles.remove(
+					message.guild.roles.cache.find(
+						(role) => role.name == `18-21`
+					)
+				);
+			});
+		} else if (hour >= 3 && hour < 6) {
+			message.guild.members.cache.map((e) => {
+				e.roles.remove(
+					message.guild.roles.cache.find((role) => role.name == `0-3`)
+				);
+			});
+		}
 	}
-	// } else if (hour >= 12 && hour < 15) {
-	// 	message.guild.roles.cache.find((role) => role.name === '9-12').delete();
-	// } else if (hour >= 15 && hour < 18) {
-	// 	client.guilds;
-	// 	// message.guild.roles.cache
-	// 	// 	.find((role) => role.name === '12-15')
-	// 	// 	.delete();
-	// } else if (hour >= 18 && hour < 21) {
-	// 	message.guild.roles.cache
-	// 		.find((role) => role.name === '15-18')
-	// 		.delete();
-	// } else if (hour >= 21 && hour < 0) {
-	// 	message.guild.roles.cache
-	// 		.find((role) => role.name === '18-21')
-	// 		.delete();
-	// } else if (hour >= 3 && hour < 6) {
-	// 	message.guild.roles.cache.find((role) => role.name === '0-3').delete();
-	// }
 
 	if (
-		message.member.roles.cache.has('782519792803381249') &&
+		message.member.roles.cache.has(adminId) &&
 		message.content.startsWith('list')
 	) {
-		const Kabe = message.guild.roles.cache.find(
-			(role) => role.name == 'Soltan'
+		const role9_12 = message.guild.roles.cache.find(
+			(role) => role.name == '9-12'
 		);
-		const Moz = message.guild.roles.cache.find(
-			(role) => role.name == 'Kabeie Mahale'
+		const role12_15 = message.guild.roles.cache.find(
+			(role) => role.name == '12-15'
+		);
+		const role15_18 = message.guild.roles.cache.find(
+			(role) => role.name == '15-18'
+		);
+		const role18_21 = message.guild.roles.cache.find(
+			(role) => role.name == '18-21'
+		);
+		const role21_0 = message.guild.roles.cache.find(
+			(role) => role.name == '21-0'
+		);
+		const role0_3 = message.guild.roles.cache.find(
+			(role) => role.name == '0-3'
 		);
 
-		const KabeMembers = message.guild.members.cache
-			.filter((member) => member.roles.cache.find((role) => role == Kabe))
-			.map((member) => member.user.tag);
-		const MozMembers = message.guild.members.cache
-			.filter((member) => member.roles.cache.find((role) => role == Moz))
-			.map((member) => member.user.tag);
-		message.channel.send(`Users with ${Kabe.name}: ${KabeMembers}`);
-		message.channel.send(`Users with ${Moz.name}: ${MozMembers}`);
+		const roleMembers9_12 = getMembers(message, role9_12);
+		const roleMembers12_15 = getMembers(message, role12_15);
+		const roleMembers15_18 = getMembers(message, role15_18);
+		const roleMembers18_21 = getMembers(message, role18_21);
+		const roleMembers21_0 = getMembers(message, role21_0);
+		const roleMembers0_3 = getMembers(message, role0_3);
+
+		message.guild.channels.cache
+			.get(botChannelId)
+			.send(`Users with ${role9_12}: ${roleMembers9_12}`);
+		message.guild.channels.cache
+			.get(botChannelId)
+			.send(`Users with ${role12_15}: ${roleMembers12_15}`);
+		message.guild.channels.cache
+			.get(botChannelId)
+			.send(`Users with ${role15_18}: ${roleMembers15_18}`);
+		message.guild.channels.cache
+			.get(botChannelId)
+			.send(`Users with ${role18_21}: ${roleMembers18_21}`);
+		message.guild.channels.cache
+			.get(botChannelId)
+			.send(`Users with ${role21_0}: ${roleMembers21_0}`);
+		message.guild.channels.cache
+			.get(botChannelId)
+			.send(`Users with ${role0_3}: ${roleMembers0_3}`);
 	}
 });
-// client.on('interactionCreate', async (interaction) => {
-// 	if (!interaction.isCommand()) return;
 
-// 	const { commandName } = interaction;
-// 	let users = new Array();
-// 	let guild = client.guilds.cache.get('782518186506453003');
-// 	let channel = client.channels.cache.get('806598220260048937');
-
-// 	guild.members.cache
-// 		.filter((member) => !member.user.bot)
-// 		.forEach((member) => users.push(member.roles));
-
-// 	if (commandName === 'list') {
-// 		await interaction.reply(`here is the list`);
-
-// 		// users.map((user) => console.log(`${user.roles}`));
-// 		// users.map((user) => channel.send(`${user.role}`));
-// 	}
-// });  // ss
 // Define Channel
 
 // Login to Discord with your client's token
